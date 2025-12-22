18c6da3d8bcfcc7d79a0609d1542bef3000057b6
Liline84 <duclesfredeline@gmail.com>
2025-12-22 15:46:05 -0500

    Update Bwa.js

--- Bwa.js
+++ Bwa.js
@@ -1,1 +1,10 @@
-/content/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/NoahBanAPK/build/outputs/apk/debug/NoahBanAPK-debug.apk to /content/bin/NoahBanAPK-0.1-arm64-v8a_armeabi-v7a-debug.apk
+Parfait ! Ça veut dire que l’APK a été copié dans le dossier *`/content/bin/`*.
+
+Tu peux maintenant le télécharger avec ce code :
+
+```python
+from google.colab import files
+files.download('/content/bin/NoahBanAPK-0.1-arm64-v8a_armeabi-v7a-debug.apk')
+```
+
+✅ Cela lancera le téléchargement de ton fichier APK final.
