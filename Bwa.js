6997716804c21c2f2708b52d6527c56615195814
Liline84 <duclesfredeline@gmail.com>
2025-12-22 15:17:37 -0500

    Create Bwa.js

---
+++ Bwa.js
@@ -0,0 +1,3 @@
+python
+from google.colab import files
+files.download('/content/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/NoahBanAPK/build/outputs/apk/debug/NoahBanAPK-0.1-arm64-v8a_armeabi-v7a-debug.apk')
