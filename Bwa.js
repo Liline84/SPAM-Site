3597831166147c1d7758400d7eaae7f6c5d1b812
Liline84 <duclesfredeline@gmail.com>
2025-12-22 15:40:02 -0500

    Update Bwa.js

--- Bwa.js
+++ Bwa.js
@@ -1,3 +1,1 @@
-python
-from google.colab import files
-files.download('/content/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/NoahBanAPK/build/outputs/apk/debug/NoahBanAPK-0.1-arm64-v8a_armeabi-v7a-debug.apk')
+/content/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/NoahBanAPK/build/outputs/apk/debug/NoahBanAPK-debug.apk to /content/bin/NoahBanAPK-0.1-arm64-v8a_armeabi-v7a-debug.apk
